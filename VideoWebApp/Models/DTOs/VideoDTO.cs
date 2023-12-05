3aed62e7fc3745fc274e0abceb04e670ff30c583
Acensti <130999724+Sylkio@users.noreply.github.com>
2023-12-05 22:13:45 +0100

    Adding foundation files

---
+++ VideoWebApp/Models/DTOs/VideoDTO.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VideoWebApp.Models.DTOs
+{
+    public class VideoDTO
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Name { get; set; } 
+        public string? VideoUrl { get; set; }
+        public string? Description { get; set; }
+        public string? ThumbnailPath { get; set; }
+        public string? Category { get; set; }
+        public TimeSpan? Duration { get; set; }
+        public int Views { get; set; }
+        public DateTime UploadDate { get; set; }
+        public string? Uploader { get; set; }
+        public List<string>? Tags { get; set; }
+    }
+}
