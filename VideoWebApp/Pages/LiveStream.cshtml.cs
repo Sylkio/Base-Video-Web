4a08e9e9bfe3e9f166a5b240a1dd1772318bbf7d
Darek <darek1430143@gmail.com>
2023-12-13 19:26:42 +0100

    finished webrtc for now

---
+++ VideoWebApp/Pages/LiveStream.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VideoWebapp.Pages
+{
+    public class LiveStreamModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
