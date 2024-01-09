ce2980c93734a645476d20c4ffc9f4cbd764f3cc
Acensti <yonas.vb223@gmail.com>
2024-01-09 17:19:38 +0100

    Implement thumbnail and gitignore fix

--- VideoWebApp/wwwroot/js/site.js
+++ VideoWebApp/wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿const thumbnailInput = document.getElementById('thumbnail');
+if (thumbnailInput.files.length > 0) {
+    formData.append('Thumbnail', thumbnailInput.files[0]);
+}
