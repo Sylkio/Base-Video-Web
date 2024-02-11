55af943f8c400176575141d6b56f1663b40614fb
Darek <darek1430143@gmail.com>
2024-02-11 22:56:30 +0100

    fixed ios record

---
+++ VideoWebApp/Pages/RecordIOS.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VideoWebapp.Pages
+{
+    public class iosrecordtestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
